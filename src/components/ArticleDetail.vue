cb523596be4b6f30c1ebf5f7811fef1e888129f7
truongpv <pham.van.truong@kloon.vn>
2021-10-19 22:05:33 +0700

    update detail blog

--- src/components/ArticleDetail.vue
+++ src/components/ArticleDetail.vue
@@ -3,20 +3,14 @@
     <div class="container">
       <h1>{{ article.title }}</h1>
 
-      <!-- <ArticleDetailMeta
-        :article="article"
-        @update="updateArticle"
-      /> -->
+      <ArticleDetailMeta :article="article" @update="updateArticle" />
     </div>
   </div>
 
   <div class="container page">
     <div class="row article-content">
       <!-- eslint-disable vue/no-v-html  -->
-      <div
-        class="col-md-12"
-        v-html="articleHandledBody"
-      />
+      <div class="col-md-12" v-html="articleHandledBody" />
       <!-- eslint-enable vue/no-v-html  -->
 
       <!-- TODO: abstract tag list component-->
@@ -31,46 +25,45 @@
       </ul>
     </div>
 
-    <hr>
+    <hr />
 
-    <!-- <div class="article-actions">
-      <ArticleDetailMeta
-        :article="article"
-        @update="updateArticle"
-      />
-    </div> -->
+    <div class="article-actions">
+      <ArticleDetailMeta :article="article" @update="updateArticle" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import marked from 'src/plugins/marked'
-import { getArticle } from 'src/services/article/getArticle'
-import { computed, defineComponent, reactive } from 'vue'
-import { useRoute } from 'vue-router'
-import ArticleDetailMeta from './ArticleDetailMeta.vue'
+import marked from "src/plugins/marked";
+import { getArticle } from "src/services/article/getArticle";
+import { computed, defineComponent, reactive } from "vue";
+import { useRoute } from "vue-router";
+import ArticleDetailMeta from "./ArticleDetailMeta.vue";
 
 export default defineComponent({
-  name: 'ArticleDetail',
+  name: "ArticleDetail",
   components: {
     ArticleDetailMeta,
   },
-  async setup () {
-    const route = useRoute()
-    const slug = route.params.slug as string
-    const article = reactive<Article>(await getArticle(slug))
+  async setup() {
+    const route = useRoute();
+    const slug = route.params.slug as string;
+    const article = reactive<Article>(await getArticle(slug));
 
-    const articleHandledBody = computed(() => marked(article.text))
+    const articleHandledBody = computed(
+      () => article.text && marked(article.text)
+    );
 
     const updateArticle = (newArticle: Article) => {
-      Object.assign(article, newArticle)
-    }
+      Object.assign(article, newArticle);
+    };
 
     return {
       article,
       articleHandledBody,
       slug,
       updateArticle,
-    }
+    };
   },
-})
+});
 </script>
