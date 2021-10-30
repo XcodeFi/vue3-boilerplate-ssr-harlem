5c9eaa306870359f5d2adfc6219e5c2223907d9e
truongpv <pham.van.truong@kloon.vn>
2021-10-30 21:47:35 +0700

    add favorite

--- src/components/ArticleDetailCommentsForm.vue
+++ src/components/ArticleDetailCommentsForm.vue
@@ -1,16 +1,10 @@
 <template>
   <p v-if="!profile">
-    <AppLink name="login">
-      Sign in
-    </AppLink> or <AppLink name="register">
-      sign up
-    </AppLink> to add comments on this article.
+    <AppLink name="login"> Sign in </AppLink> or
+    <AppLink name="register"> sign up </AppLink> to add comments on this
+    article.
   </p>
-  <form
-    v-else
-    class="card comment-form"
-    @submit.prevent="submitComment"
-  >
+  <form v-else class="card comment-form" @submit.prevent="submitComment">
     <div class="card-block">
       <textarea
         v-model="comment"
@@ -21,10 +15,7 @@
       />
     </div>
     <div class="card-footer">
-      <img
-        :src="profile.profilePicUrl"
-        class="comment-author-img"
-      >
+      <img :src="profile.profilePicUrl" class="comment-author-img" />
       <button
         aria-label="Submit"
         type="submit"
@@ -38,39 +29,41 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, computed } from 'vue'
+import { defineComponent, ref, computed } from "vue";
 
-import { useProfile } from '../composable/useProfile'
+import { useProfile } from "../composable/useProfile";
 
-import { postComment } from '../services/comment/postComment'
+import { postComment } from "../services/comment/postComment";
 
-import { user, checkAuthorization } from '../store/user'
+import { user, checkAuthorization } from "../store/user";
 
 export default defineComponent({
-  name: 'ArticleDetailCommentsForm',
+  name: "ArticleDetailCommentsForm",
   props: {
     articleSlug: { type: String, required: true },
   },
   emits: {
-    'add-comment': (comment: ArticleComment) => !!comment.id,
+    "add-comment": (comment: ArticleComment) => !!comment._id,
   },
-  setup (props, { emit }) {
-    const username = computed(() => checkAuthorization(user) ? user.value.email : '')
-    const { profile } = useProfile({ username })
+  setup(props, { emit }) {
+    const username = computed(() =>
+      checkAuthorization(user) ? user.value.email : ""
+    );
+    const { profile } = useProfile({ username });
 
-    const comment = ref('')
+    const comment = ref("");
 
     const submitComment = async () => {
-      const newComment = await postComment(props.articleSlug, comment.value)
-      emit('add-comment', newComment)
-      comment.value = ''
-    }
+      const newComment = await postComment(props.articleSlug, comment.value);
+      emit("add-comment", newComment);
+      comment.value = "";
+    };
 
     return {
       profile,
       comment,
       submitComment,
-    }
+    };
   },
-})
+});
 </script>
