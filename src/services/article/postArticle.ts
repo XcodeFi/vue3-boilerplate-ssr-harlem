e866886d868f5aa3190c339d49ec12d7d7122539
xcodefi <phdfieldkidpro@gmail.com>
2022-02-06 22:22:13 +0700

    add article

--- src/services/article/postArticle.ts
+++ src/services/article/postArticle.ts
@@ -1,3 +1,4 @@
+import { Either, fail, success } from 'src/utils/either'
 import requestAuthorize, { request } from '../index'
 
 interface PostArticleForm {
@@ -19,6 +20,41 @@ export function postArticle (form: PostArticleForm): Promise<Article> {
     tags: form.tags,
   })
     .then(res => res.data)
+}
+
+export async function postArticleGraphql (form: PostArticleForm):
+Promise<Either<Error[], Article>> {
+  const variables = {
+    query: `mutation CreateArticle {
+      createArticle(input:
+        {
+          title: "${form.title}",
+          description: "${form.description}",
+          text: "${form.text}",
+          blogUrl: "${form.blogUrl}",
+          tags: ${JSON.stringify(form.tags)},
+        }) {
+          id
+          title
+          description
+          draftText
+          tags
+          text
+          imgUrl
+          blogUrl
+          likes
+          score
+          isSubmitted
+      }
+    }`,
+  }
+
+  const result = await request.checkablePostGraphql<{
+    createArticle: Article
+  }>(variables)
+
+  if (result.isOk()) return success(result.value.createArticle)
+  else return fail(result.value)
 }
 
 export function putArticle (slug: string, form: PostArticleForm): Promise<Article> {
